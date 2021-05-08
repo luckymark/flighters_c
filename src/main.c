a7df74a5877aa1e12599310579edf6dcb090b9e0
fengzhigang <fengzg@polyvi.com>
2021-05-08 16:34:13 +0800

    feature:播放背景音乐

--- src/main.c
+++ src/main.c
@@ -8,6 +8,10 @@ int main() {
     const int screen_height = 800;
 
     InitWindow(screen_width, screen_height, "Flighters");
+    InitAudioDevice();
+
+    Music music = LoadMusicStream("resources/sound/game_music.ogg");
+    PlayMusicStream(music);
 
     Texture2D texture = LoadTexture("resources/image/shoot.png");
     Texture2D sky = LoadTexture("resources/image/background.png");
@@ -19,13 +23,18 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
 
+        UpdateMusicStream(music);
+
         draw_sky(&sky);
         draw_hero(hero);
 
         EndDrawing();
     }
 
+    UnloadMusicStream(music);
+    CloseAudioDevice();
     CloseWindow();
+
     return 0;
 }
 
